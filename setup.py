158b45fddc08ae22c9d40c2fcea99e2adae2054a
mina@UbuntuSubsystemForWindows <minamaged113@gmail.com>
2019-06-30 18:38:38 +0300

    [settings] adds ´MANIFEST.in´ and edits ´requirements.txt´

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SMSONAR",
+    version="0.0.1",
+    author="Mina Gabriel",
+    author_email="minamaged113@gmail.com",
+    description="Sound Metrics SONARs files software development kit \
+    (SDK) for reading and writing SONAR files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minamaged113/SMSONAR",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+)
