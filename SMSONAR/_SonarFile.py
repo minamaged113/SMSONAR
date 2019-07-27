73548901f86e86526c5246625eb9452ae1277bbd
mina@UbuntuSubsystemForWindows <minamaged113@gmail.com>
2019-07-27 23:47:34 +0300

    [test][code] [test] editing tests. [code] edits code.

--- SMSONAR/_SonarFile.py
+++ SMSONAR/_SonarFile.py
@@ -1,3 +1,13 @@
+VALUE_ERROR_MSG = 'Unexpected type of input for {}. Expected {}, but {} \
+was given.'
+
+
 class SonarFile:
-    def __init__(self):
+    def __init__(self, filePath: str = None):
+        if not isinstance(filePath, str) and filePath is not None:
+            # check if the passed path is a string
+            # if not, raise a ValueError
+            raise ValueError(VALUE_ERROR_MSG.format('filePath',
+                                                    'string',
+                                                    'integer'))
         return
--- tests/test_SonarFile.py
+++ tests/test_SonarFile.py
@@ -1,10 +1,35 @@
-import sys
-import os
-sys.path.append(os.getcwd())
-import pytest 
 import SMSONAR._SonarFile as SF
+import pytest
+
+
+SonarFileSamplePath = 'FilePath'
+SonarFileSamplePathInt = 3
 
 
 def test_test():
-    _=SF.SonarFile()
     assert True
+
+
+def test_call_file_super_class():
+    assert bool(SF.SonarFile()) is True
+
+
+def test_open_sonar_file():
+    assert bool(SF.SonarFile(SonarFileSamplePath)) is True
+
+
+def test_open_sonar_file_passing_none_str_input_as_file_name_fails_to_open():
+    with pytest.raises(ValueError) as e:
+        assert SF.SonarFile(SonarFileSamplePathInt)
+    assert str(e.value) == ('Unexpected type of input for filePath.' +
+                            ' Expected string, but integer was given.')
+
+
+# def test_open_sonar_file_given_invalid_path_fails():
+#     assert False
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    sys.path.append(os.getcwd())
