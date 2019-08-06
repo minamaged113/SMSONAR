3a0189ac3618d95f80211d8ec5b3892fd87928b7
mina@ubuntu@lenovoZ5070 <minamaged113@gmail.com>
2019-08-06 18:34:29 +0300

    [code][test] [code] encapsulates messages and tags. [test] adds documentation and skips one unimplemented test.

--- SMSONAR/_SonarFile.py
+++ SMSONAR/_SonarFile.py
@@ -1,13 +1,13 @@
-VALUE_ERROR_MSG = 'Unexpected type of input for {}. Expected {}, but {} \
+class SonarFile:
+    __TAG = 'SonarFile'
+    __VALUE_ERROR_MSG = 'Unexpected type of input for {}. Expected {}, but {} \
 was given.'
 
-
-class SonarFile:
     def __init__(self, filePath: str = None):
         if not isinstance(filePath, str) and filePath is not None:
             # check if the passed path is a string
             # if not, raise a ValueError
-            raise ValueError(VALUE_ERROR_MSG.format('filePath',
+            raise ValueError(self.__VALUE_ERROR_MSG.format('filePath',
                                                     'string',
                                                     'integer'))
         return
--- tests/test_SonarFile.py
+++ tests/test_SonarFile.py
@@ -1,13 +1,22 @@
+"""
+SonarFile class test module
+===========================
+SonarFile is a parent (super) class where child classes inherit from. It
+resembles a generic Sound Metric Sonar file. However, child classes
+should be specific file versions.
+This module tests the following:
+
+-[] SonarFile class can be instantiated.
+-[] Given string python path, class is created.
+-[] Given 
+"""
+
 import SMSONAR._SonarFile as SF
 import pytest
 
 
 SonarFileSamplePath = 'FilePath'
 SonarFileSamplePathInt = 3
-
-
-def test_test():
-    assert True
 
 
 def test_call_file_super_class():
@@ -24,12 +33,6 @@ def test_open_sonar_file_passing_none_str_input_as_file_name_fails_to_open():
     assert str(e.value) == ('Unexpected type of input for filePath.' +
                             ' Expected string, but integer was given.')
 
-
-# def test_open_sonar_file_given_invalid_path_fails():
-#     assert False
-
-
-if __name__ == "__main__":
-    import os
-    import sys
-    sys.path.append(os.getcwd())
+@pytest.mark.skip
+def test_open_sonar_file_given_invalid_path_fails():
+    assert False
